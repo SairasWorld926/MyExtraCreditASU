c7c19054d5e5b148bf0c88686cbe884ab1242f8c
Saira Hernandez <Sairah48@gmail.com>
2020-10-05 20:22:51 -0400

    Changed MyPyessay.md

---
+++ MyPyscript.py
@@ -0,0 +1,9 @@
+def greet(name):
+    """
+    This function greets the person
+    who is passed in a parameter
+    """
+    print("Hello there, " + name + ". Nice to finally meet you!")
+
+greet('Cristian Vallejo')
+
